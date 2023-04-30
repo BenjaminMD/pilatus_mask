fbd401ea2d27e894a1efe7878c20a4cad627d759
Benjamin Fahl <Benjamin.Fahl@rwth-aachen.de>
2023-04-30 22:54:13 +0200

    tidy

--- mask.py
+++ mask.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import toml
 
 class PilatusMask:
     def __init__(self):
         self.x_size, self.y_size = 1679, 1475
+        self.pixel_size = 1.72e-4
         self.e = 1
+        self.x_len = self.x_size * self.pixel_size
+        self.y_len = self.y_size * self.pixel_size
         self.regions = np.array([[488, 494, None, None],
                                  [982, 988, None, None],
                                  [None, None, 196, 212],
@@ -36,6 +40,14 @@ class PilatusMask:
         fig, ax = plt.subplots()
         ax.imshow(self.mask, cmap='binary', origin='lower', extent=[0, self.x_size, 0, self.y_size], aspect='equal')
         ax.set(xlabel='$x$ Pixel / -', ylabel='$y$ Pixel / -')
+
+        if self.p1:
+            ax.axvline(self.p1/self.x_len * self.x_size, color='r', linestyle='--')
+            ax.axhline(self.p2/self.y_len * self.y_size, color='r', linestyle='--')
+
+            # plot from p1 p2 to corner
+            print(self.corner)
+            ax.plot([self.p1/self.x_len * self.x_size, self.corner[0]], [self.p2/self.y_len * self.y_size, self.corner[1]], color='r', linestyle='--')
         plt.show()
     
     def sub_grid_masking(self):
@@ -47,7 +59,29 @@ class PilatusMask:
     def combine_mask(self, path_to_mask):
         mask = np.load(path_to_mask)
         self.mask = np.logical_or(self.mask, mask).astype(int)
+
+    def read_poni(self, path_to_poni):
+        with open(path_to_poni, 'r') as f:
+            p1, p2 = f.readlines()[6:8]
+        self.p1 = float(*p1.split()[1:])
+        self.p2 = float(*p2.split()[1:])
+        self.identify_furthest_corner()
         
+
+        print(p1, p2)
+
+    def identify_furthest_corner(self):
+        c1 = np.array([self.x_size, self.y_size])
+        c2 = np.array([0, 0])
+        c3 = np.array([self.x_size, 0])
+        c4 = np.array([0, self.y_size])
+
+        corners = np.array([c1, c2, c3, c4])
+        self.pixel_p1 = self.p1 / self.x_len * self.x_size
+        self.pixel_p2 = self.p2 / self.y_len * self.y_size
+        distances = np.linalg.norm(corners - np.array([self.pixel_p1, self.pixel_p2]), axis=1)
+        self.corner = corners[np.argmax(distances)]
+
 
 def main():
     mask = PilatusMask()
