9f9c314a44998935ddc2cf9da083176015ec363e
Benjamin Fahl <Benjamin.Fahl@rwth-aachen.de>
2023-05-28 19:30:37 +0200

    removed submodule

--- mask.py
+++ mask.py
@@ -28,13 +28,13 @@ class PilatusMask:
         self.mask = np.zeros((self.x_size, self.y_size))
         for y1, y2, x1, x2 in self.regions:
             if y1 is None and y2 is None:
-                self.mask[x1-1-self.e:x2+self.e,:] = +1
+                self.mask[x1-1-self.e:x2+self.e, :] = +1
             else:
-                self.mask[:,y1-1-self.e:y2+self.e] = +1
-    
+                self.mask[:, y1-1-self.e:y2+self.e] = +1
+
     def save_mask(self, filename):
         np.save(filename, self.mask)
-    
+
     def plot_mask(self):
         fig, ax = plt.subplots()
         ax.imshow(self.mask, cmap='binary', origin='lower', extent=[0, self.x_size, 0, self.y_size], aspect='equal')
@@ -48,12 +48,12 @@ class PilatusMask:
          #   print(self.corner)
           #  ax.plot([self.p1/self.x_len * self.x_size, self.corner[0]], [self.p2/self.y_len * self.y_size, self.corner[1]], color='r', linestyle='--')
         plt.show()
-    
-    def sub_grid_masking(self):
+ 
+    def sub_grid_masking(self, e):
         for x in self.x_submodules:
-            self.mask[:,x-2:x+1] = +1
+            self.mask[:,x-1-e:x+e] = +1
         for y in self.y_submodules:
-            self.mask[y-2:y+1,:] = +1
+            self.mask[y-1-e:y+e,:] = +1
 
     def mask_edges(self):
         """adds edges to mask"""
